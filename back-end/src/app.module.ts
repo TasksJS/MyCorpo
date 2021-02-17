219986e68346549b6926cbd651a3cca295697380
Guillaume Chateauroux <guillaume.chateauroux@gmail.com>
2021-02-17 11:52:42 +0100

    feat(config): Added project env configuration with database and typeorm for mysql

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: configuration().database.host,
+      port: configuration().database.port,
+      username: configuration().database.user,
+      password: configuration().database.password,
+      database: configuration().database.database,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ back-end/src/config/configuration.ts
@@ -0,0 +1,9 @@
+export default () => ({
+  database: {
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DATABASE,
+  },
+});
