ecb015c9f208e092dbed30d9cfbbfbb7c251d183
Benjamin FERIE <benjamin.ferie@ynov.com>
2021-02-17 12:18:35 +0100

    feat: Ajout de la page de connexion
    Ajout de la page de connexion, uniquement le formulaire, sans traitement

---
+++ front-end/pages/connect.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-row>
+    <v-col class="text-center" cols="6" offset="3">
+      <img
+        src="/v.png"
+        alt="Vuetify.js"
+        class="mb-5"
+      >
+      <v-card>
+        <v-card-text>
+          <v-form
+            ref="form"
+            v-model="valid"
+            lazy-validation
+          >
+            <v-text-field
+              v-model="name"
+              label="Login"
+              required
+            />
+            <v-text-field
+              v-model="password"
+              type="password"
+              label="Mot de passe"
+              required
+            />
+            <v-btn tile>
+              Se connecter
+            </v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
