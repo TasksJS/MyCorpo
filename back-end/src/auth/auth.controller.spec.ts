b5c3ec5e35911866141a5129cab434492280d3fd
Nicolas Barbosa <nicolas.barbosa@orange.com>
2021-03-09 11:35:22 +0100

    feature(add-unit-test): Added initials test on services and controllers

--- back-end/src/auth/auth.controller.spec.ts
+++ back-end/src/auth/auth.controller.spec.ts
@@ -1,15 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController],
-    }).compile();
-
-    controller = module.get<AuthController>(AuthController);
+    controller = new AuthController(authService);
   });
 
   it('should be defined', () => {
