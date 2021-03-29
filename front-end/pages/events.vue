c9deb7afe9d0c435b79f09b73f9043018c6c9ebc
theejkb <steven.copy@ynov.com>
2021-03-29 16:13:02 +0200

    feat(cypress): Added integration test with Cypress

--- front-end/pages/events.vue
+++ front-end/pages/events.vue
@@ -5,11 +5,9 @@
         <MyCorpoLogo />
       </div>
       <v-card>
-        <v-card-title class="headline">
-          Évènements
-        </v-card-title>
+        <v-card-title class="headline"> Évènements </v-card-title>
         <v-card-text>
-          <v-card v-for="(event) in filteredAndSortedEvents" :key="event.id">
+          <v-card v-for="event in filteredAndSortedEvents" :key="event.id">
             <v-card-title>
               {{ event.dateFormated }} : {{ event.name }}
             </v-card-title>
@@ -19,25 +17,13 @@
               </v-col>
               <v-col cols="12" class="text-right">
                 <v-btn>
-                  <v-icon>
-                    mdi-pencil-outline
-                  </v-icon>
+                  <v-icon> mdi-pencil-outline </v-icon>
                   Modifier
                 </v-btn>
-                <v-dialog
-                  v-model="dialog"
-                  width="500"
-                >
+                <v-dialog v-model="dialog" width="500">
                   <template #activator="{ on, attrs }">
-                    <v-btn
-                      color="red darken-2"
-                      dark
-                      v-bind="attrs"
-                      v-on="on"
-                    >
-                      <v-icon>
-                        mdi-delete-outline
-                      </v-icon>
+                    <v-btn color="red darken-2" dark v-bind="attrs" v-on="on">
+                      <v-icon> mdi-delete-outline </v-icon>
                       Supprimer
                     </v-btn>
                   </template>
@@ -48,7 +34,10 @@
                     </v-card-title>
 
                     <v-card-text>
-                      <span>Êtes-vous sûr de vouloir supprimer cet évènement ?</span>
+                      <span
+                        >Êtes-vous sûr de vouloir supprimer cet évènement
+                        ?</span
+                      >
                     </v-card-text>
 
                     <v-divider />
@@ -69,14 +58,18 @@
               </v-col>
             </v-card-text>
           </v-card>
-          <hr class="my-3">
+          <hr class="my-3" />
         </v-card-text>
         <v-card-actions>
           <v-row justify="center" align="center">
             <v-col cols="12" sm="8" md="6">
               <v-card>
                 <v-card-text>
-                  <div v-if="errors.length" class="text-center" style="color: red">
+                  <div
+                    v-if="errors.length"
+                    class="text-center"
+                    style="color: red"
+                  >
                     <b>Des champs ne sont pas remplis:</b>
                     <ul style="list-style-type: none">
                       <li v-for="error in errors" :key="error">
@@ -84,9 +77,7 @@
                       </li>
                     </ul>
                   </div>
-                  <v-form
-                    ref="form"
-                  >
+                  <v-form ref="form">
                     <v-text-field
                       v-model="name"
                       label="Titre"
@@ -114,15 +105,11 @@
                           label="Date de l'évènement"
                           prepend-icon="mdi-calendar"
                           name="date"
-                          readonly
                           v-bind="attrs"
                           v-on="on"
                         />
                       </template>
-                      <v-date-picker
-                        v-model="date"
-                        @input="menu = false"
-                      />
+                      <v-date-picker v-model="date" @input="menu = false" />
                     </v-menu>
                     <v-menu
                       ref="menu"
@@ -142,7 +129,6 @@
                           label="Heure de l'évènement"
                           prepend-icon="mdi-clock-time-four-outline"
                           name="time"
-                          readonly
                           v-bind="attrs"
                           v-on="on"
                         />
@@ -200,7 +186,9 @@ export default {
       const filtered = vm.events.filter((event) => {
         return event.isActive
       })
-      return filtered.sort((a, b) => moment(a.date).isAfter(moment(b.date)) ? 1 : -1)
+      return filtered.sort((a, b) =>
+        moment(a.date).isAfter(moment(b.date)) ? 1 : -1
+      )
     }
   },
   methods: {
@@ -208,16 +196,16 @@ export default {
       this.errors = []
 
       if (!this.name) {
-        this.errors.push('Aucun nom d\'évènement.')
+        this.errors.push("Aucun nom d'évènement.")
       }
       if (!this.description) {
-        this.errors.push('Aucune description d\'évènement.')
+        this.errors.push("Aucune description d'évènement.")
       }
       if (!this.date) {
-        this.errors.push('Aucune date d\'évènement.')
+        this.errors.push("Aucune date d'évènement.")
       }
       if (!this.hour) {
-        this.errors.push('Aucune heure d\'évènement.')
+        this.errors.push("Aucune heure d'évènement.")
       }
     },
     async submitEvent () {
@@ -236,7 +224,10 @@ export default {
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(eventToPost)
         }
-        const response = await fetch('http://localhost:3333/evenement', requestOptions)
+        const response = await fetch(
+          'http://localhost:3333/evenement',
+          requestOptions
+        )
         const data = await response
         if (data.status === 201) {
           this.events.push(eventToPost)
@@ -249,16 +240,16 @@ export default {
     },
     deleteEvent (event) {
       this.dialog = false
-      this.$axios.$delete('evenement/' + event.id)
-        .then(() => {
-          const index = this.events.map((event) => {
+      this.$axios.$delete('evenement/' + event.id).then(() => {
+        const index = this.events
+          .map((event) => {
             return event.id
-          }).indexOf(event.id)
+          })
+          .indexOf(event.id)
 
-          this.events.splice(index, 1)
-        })
+        this.events.splice(index, 1)
+      })
     }
   }
-
 }
 </script>
