43a88c4641f7246382a4af8d781eab02aff4ea1b
Nicolas Barbosa <nicolas.barbosa@orange.com>
2021-03-29 12:08:43 +0200

    feat(develop): Added tests

--- back-end/src/auth/auth.service.spec.ts
+++ back-end/src/auth/auth.service.spec.ts
@@ -1,17 +1,45 @@
 import { AuthService } from './auth.service';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import * as request from 'supertest';
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { AuthModule } from './auth.module';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
   let userService: UsersService;
   let jwtService: JwtService;
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AuthModule],
+    })
+      .overrideProvider(AuthService)
+      .useValue(authService)
+      .compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
 
   beforeEach(() => {
-    service = new AuthService(userService, jwtService);
+    authService = new AuthService(userService, jwtService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  it('Should be log', () => {
+    return request(app.getHttpServer())
+      .post('/login')
+      .send({ username: 'test', password: 'azerty' })
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return err;
+        return;
+      });
   });
 });
